908a2031c5ee7c048bd05d8a7f8db4b688030f0e
GameZoneMusic <39338716+GameZoneMusic@users.noreply.github.com>
2018-05-21 15:15:54 +0700

    Update MusicBot.js

--- MusicBot.js
+++ MusicBot.js
@@ -25,10 +25,7 @@ client.on('reconnecting', () => console.log('I am reconnecting now!'));
 client.on('message', async msg => { // eslint-disable-line
 	if (msg.author.bot) return undefined;
 	if (!msg.content.startsWith(prefix)) return undefined;
-	
-	client.user.setUsername("AVmusic")
-        client.user.setActivity('Alfian Verter', {type: 'STREAMING'})
-	
+		
 	const args = msg.content.split(' ');
 	const searchString = args.slice(1).join(' ');
 	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
